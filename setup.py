1f6e1259f640dd88da2a4f2e76d9af1fe1219bfa
Nikita Kretov <nkretov@croc.ru>
2020-01-31 15:57:33 +0300

    remove optional kerb feauture

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
     requires = [
         'python-dateutil',
         'requests',
-        'requests-kerberos',
+        #'requests-kerberos',
         'six',
         #'libcomps',
         #'rpm-py-installer', # it is optional feature
